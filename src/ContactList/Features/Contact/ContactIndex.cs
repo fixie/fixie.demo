4900383310ebbf1686ce655147601eda14695391
Patrick Lioi <patrick.lioi@gmail.com>
2019-10-21 17:54:17 -0500

    The ContactIndex view model properly identifies which of its properties are nullable.

--- src/ContactList/Features/Contact/ContactIndex.cs
+++ src/ContactList/Features/Contact/ContactIndex.cs
@@ -12,9 +12,16 @@
 
         public class ViewModel
         {
+            public ViewModel(string name, string email, string? phoneNumber = null)
+            {
+                Name = name;
+                Email = email;
+                PhoneNumber = phoneNumber;
+            }
+
             public Guid Id { get; set; }
-            public string? Name { get; set; }
-            public string? Email { get; set; }
+            public string Name { get; set; }
+            public string Email { get; set; }
             public string? PhoneNumber { get; set; }
         }
 
